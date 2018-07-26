e746bbb2366dcaed241ebf27f0f2b1242fb187da
shreyabindal <shreya.bindal@gmail.com>
2018-07-26 21:04:21 +0530

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,9 @@
+package Game;
+
+public class Client {
+
+	public static void main(String[] args) {
+		TypingTutor tutor = new TypingTutor(
+				"Formed mostly as the result of volcanic eruption Java is the 13th largest island in the world and the fifth largest in Indonesia by landmass A chain of volcanic mountains forms an east–west spine along the island Three main languages are spoken on the island Javanese Sundanese and Madurese Of these Javanese is the dominant it is the native language of about 60 million people in Indonesia most of whom live on Java Furthermore most residents are bilingual speaking Indonesian the official language of Indonesia as their first or second language While the majority of the people of Java are Muslim Java population is a diverse mixture of religious beliefs ethnicities and cultures");
+	}
+}
